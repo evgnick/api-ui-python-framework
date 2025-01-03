7a6dd08abfc76cb4daf20e20ec702120685ee9d0
YevgenyN <evgenii.tst@gmail.com>
2025-01-03 15:06:19 +0300

    Add allure and screenshot

--- pages/login_page.py
+++ pages/login_page.py
@@ -14,12 +14,12 @@ class LoginPage(BasePage):
         super().__init__(driver)
 
     def enter_login(self, login):
-        with allure.step(f"Enter login"):
+        with allure.step("Enter login"):
             self.wait.until(ec.element_to_be_clickable(self._USERNAME_FIELD)).send_keys(login)
             return self
 
     def enter_password(self, password):
-        with allure.step(f"Enter password"):
+        with allure.step("Enter password"):
             self.wait.until(ec.element_to_be_clickable(self._PASSWORD_FIELD)).send_keys(password)
             return self
 
